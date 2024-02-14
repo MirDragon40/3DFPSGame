3129e9b52aa7c749e01edd7be0e77020d4ad4cb6
DESKTOP-358CSLP\USER <mirjeong6463@gmail.com>
2024-02-14 15:03:26 +0900

    플레이어 점프, 중력 구현

--- 3DFPSGame/Assets/02.Scripts/Player/PlayerMove.cs
+++ 3DFPSGame/Assets/02.Scripts/Player/PlayerMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -22,6 +23,27 @@ public class PlayerMove : MonoBehaviour
     public Slider StaminaSliderUI;
 
     private CharacterController _characterController;
+
+    // 목표: 스페이스 바를 누르면 캐릭터를 점프하고 싶다. 
+    // 필요 속성:
+    // - 점프 파워 값
+    public float JumpPower = 10f;
+
+    // 구현 순서:
+    // 1. 만약에 [Spacebar] 버튼을 누르는 순간 && 땅이면...
+    
+    // 2. 플레이어에게 y축에 있어 점프 파워를 적용한다. 
+
+
+    // 목표: 캐릭터에게 중력을 적용하고 싶다. 
+    // 필요속성: 
+    // - 중력 값
+    private float _gravity = -20;  // 중력 변수
+    // - 누적할 중력 변수: y축 속도
+    private float _yVelocity = 0f;
+    // 구현 순서:
+    // 1. 중력 가속도가 누적된다. 
+    // 2. 플레이어에게 y축에 있어 중력을 적용한다.
 
     private void Awake()
     {
@@ -69,7 +91,23 @@ public class PlayerMove : MonoBehaviour
         Stamina = Mathf.Clamp(Stamina, 0, 100);
         StaminaSliderUI.value = Stamina / MaxStamina;  // 0 ~ 1;//
 
-        // 3. 이동하기
+        // 점프 구현
+        // 1. 만약에 [Spacebar] 버튼을 누르는 순간 && 땅이면...
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)  
+        {
+            // 2. 플레이어에게 y축에 있어 점프 파워를 적용한다. 
+            _yVelocity = JumpPower;
+        }
+
+
+
+        // 3-1. 중력 적용
+        // 1. 중력 가속도가 누적된다. 
+        _yVelocity = _yVelocity + _gravity * Time.deltaTime;
+        // 2. 플레이어에게 y축에 있어 중력을 적용한다.
+       dir.y = _yVelocity;
+
+        // 3-2. 이동하기
         //transform.position += speed * dir * Time.deltaTime;
         _characterController.Move(motion: dir * speed * Time.deltaTime);
 
