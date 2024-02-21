45a12ef3ec187c97e139c614a8cc7d7fa2907e91
DESKTOP-358CSLP\USER <mirjeong6463@gmail.com>
2024-02-21 16:05:23 +0900

    아이템 매니저 작성중3

--- 3DFPSGame/Assets/02.Scripts/Item/ItemManager.cs
+++ 3DFPSGame/Assets/02.Scripts/Item/ItemManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // 역할: 아이템들을 관리해주는 관리자
 // 데이터 관리 -> 데이터(여기에서는 아이템)를 생성, 수정, 삭제, 조회(검색)  // 또는 정렬
@@ -8,6 +9,12 @@ using UnityEngine;
 public class ItemManager : MonoBehaviour
 {
     public ItemManager Instance { get; private set; }
+
+    public Text HealthItemCountTextUI;
+    public Text StaminaItemCountTextUI;
+    public Text BulletItemCountTextUI;
+
+
 
 
     private void Awake()
@@ -24,11 +31,15 @@ public class ItemManager : MonoBehaviour
 
     public List<Item> ItemList = new List<Item>();   // 아이템 리스트
 
+
     private void Start()
     {
         ItemList.Add(new Item(ItemType.Health, 1));   // 0: Health
         ItemList.Add(new Item(ItemType.Stamina, 1));   // 1: Stamina
         ItemList.Add(new Item(ItemType.Bullet, 1));   // 2: Bullet
+
+        RefreshUI();
+
 
     }
 
@@ -70,5 +81,13 @@ public class ItemManager : MonoBehaviour
             }
         }
         return false;
+    }
+
+    // UI를 새로고침 하는 함수
+    public void RefreshUI()
+    {
+        HealthItemCountTextUI.text = $"x{GetItemcount(ItemType.Health)}";
+        StaminaItemCountTextUI.text = $"x{GetItemcount(ItemType.Stamina)}";
+        BulletItemCountTextUI.text = $"x{GetItemcount(ItemType.Bullet)}";
     }
 }
